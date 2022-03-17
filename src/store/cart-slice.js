4a50cf97c947cee0d77b2f24ea108cf53dd2d937
Sean <seansmodd@gmail.com>
2022-03-17 15:34:02 -0700

    another great learning starting point is here

---
+++ src/store/cart-slice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  itemsList: [],
+  totalQuantity: 0,
+  showCart: false,
+  totalPrice: 0,
+};
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {},
+    removeFromCart: (state, action) => {},
+    setShowCart: (state) => {
+      state.showCart = !state.showCart;
+    },
+  },
+});
+
+export const { reducerName } = cartSlice.actions;
+
+export default cartSlice.reducer;
