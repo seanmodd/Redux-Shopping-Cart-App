b841ed692fd8ccb7b577131487e608fbb0deee44
Sean <seansmodd@gmail.com>
2022-03-17 14:42:31 -0700

    AT THIS POINT. YOU MUST KNOW WHAT TO DO.

--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 
 import './Auth.css';
 
-const Auth = () => (
-  <div className="container">
-    <h1>Login</h1>{' '}
-    <form>
-      <label htmlFor="id">Id</label>
-      <input type="text" name="id" id="id" />
-      <label htmlFor="password">Password</label>
-      <input type="password" name="password" id="password" />
-      <button className="login-btn" type="submit">
-        Login
-      </button>
-    </form>
-  </div>
-);
+const Auth = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('submit');
+  };
+
+  return (
+    <div className="container">
+      <h1>Login</h1>{' '}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="id">Id</label>
+        <input type="text" name="id" id="id" />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+        <button className="login-btn" type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Auth;
