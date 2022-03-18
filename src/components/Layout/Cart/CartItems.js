cceff53b64489fce619186bf81472912d15fceef
Sean <seansmodd@gmail.com>
2022-03-18 14:27:18 -0700

    now showing cart below

--- src/components/Layout/Cart/CartItems.js
+++ src/components/Layout/Cart/CartItems.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import CartItem from './CartItem';
 import './Cart.css';
 
-const CartItems = () => (
-  <div className="cart-container">
-    <h2>Your Cart</h2>
-    <ul>
-      <li>
-        <CartItem id={1} price={2500} name="Macbook" />
-      </li>
-    </ul>
-  </div>
-);
+const CartItems = () => {
+  const cartItems = useSelector((state) => state.cart.itemsList);
+  return (
+    <div className="cart-container">
+      <h2>Your Cart</h2>
+      <ul>
+        <li>
+          <CartItem id={1} price={2500} name="Macbook" />
+        </li>
+        {cartItems.map((item) => (
+          <li key={item.id}>
+            <CartItem id={item.id} price={item.price} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default CartItems;
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Products from './Products';
 import './Layout.css';
+import CartItems from './Cart/CartItems';
 
 const Layout = () => {
   const total = 100;
@@ -10,6 +11,7 @@ const Layout = () => {
     <div className="layout">
       <Header />
       <Products />
+      <CartItems />
       <div className="total-price">
         <h3>Total: ${total}</h3>
         <button className="orderBtn" type="submit">
